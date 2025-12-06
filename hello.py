11065e038baf99d2fbe1cfbf681010c11cc9af9a
PlzShoot <hoai.nguyenkhdlk25@hcmut.edu.vn>
2025-12-06 10:52:03 +0700

    Initial commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+print(2+5)
+message = "This is a simple Python script."
+message.upper()
+print(message)
---
+++ library_test.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [10, 20, 25, 22])
+plt.title("Test Plot")
+plt.xlabel("X")
+plt.ylabel("Y")
+
+plt.show()
+print("Plot displayed successfully.")
